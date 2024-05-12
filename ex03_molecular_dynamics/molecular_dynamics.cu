f46f3db810654264e65b1187c1f986e93c33cf11
Rifat N <rifat.nas.07@gmail.com>
2024-05-12 14:11:47 +0200

    redone data manipulation

--- ex03_molecular_dynamics/molecular_dynamics.cu
+++ ex03_molecular_dynamics/molecular_dynamics.cu
@@ -5,7 +5,7 @@
 #include <string>
 #include <iomanip>
 
-struct molecule
+struct Molecule
 {
     double mass;
     double x;
@@ -21,45 +21,84 @@ struct molecule
 
 struct Force
 {
-    double x;
-    double y;
-    double z;
+    double x = 0;
+    double y = 0;
+    double z = 0;
 };
 
-std::vector<molecule> fill_points(int, std::ifstream &);
+void fill_points(Molecule*, int, std::ifstream &);
 void get_values(std::vector<double> &, std::string);
-void writeVTK(const char* name, int num_points, std::vector<molecule> points);
+void writeVTK(int index, int num_molecules, Molecule *points);
+
+__global__ void change_data(Molecule *data, int num_molecules, double time_step){
+
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if (index < num_molecules){
+        data[index].x += time_step;
+        data[index].y += time_step;
+        data[index].z += time_step;
+        }
+}
 
 
 int main(int argc, char *argv[]){
+    
+    double time_step = atof(argv[1]);
+    int num_steps = atoi(argv[2]);
+    double delta = atof(argv[3]);
+    double eps = atof(argv[4]);
+
+     int num_molecules = 0;
+    
     std::ifstream file;
     file.open("points.txt");
-
-    double time_step = atoi(argv[1]);
-    int num_steps = atoi(argv[2]);
-    double delta = atoi(argv[3]);
-    double eps = atoi(argv[4]);
-
-    int num_points = 0;
 
     if (file.is_open()){
         std::string line;
         std::getline(file, line);
-        num_points = std::stoi(line);    
+        num_molecules = std::stoi(line);    
     }
-    std::vector<molecule> grid = fill_points(num_points, file);   
+
+    int size_molecule = num_molecules * sizeof(Molecule);
+    int size_forces =  num_molecules * sizeof(Force);
+
+    Molecule *grid_host, *grid_device;
+    Force *forces_host, *forces_device;
+
+    cudaMallocHost(&grid_host, size_molecule);
+    cudaMalloc(&grid_device, size_molecule);
+
+    cudaMallocHost(&forces_host, size_forces);
+    cudaMalloc(&forces_device, size_forces);
+
+    fill_points(grid_host, num_molecules, file);
     file.close();
 
-    // here do steps
+    cudaMemcpy(grid_device, grid_host, size_molecule, cudaMemcpyHostToDevice);
+    cudaMemcpy(forces_device, forces_host, size_forces, cudaMemcpyHostToDevice);
 
 
 
-    writeVTK("output.vtk", num_points, grid);
+    int NUM_THREAD = 5;
+    int NUM_BLOCK = 1;
+    for(int i = 0; i < num_steps; i++){
+        change_data<<<NUM_BLOCK, NUM_THREAD>>>(grid_device, num_molecules, time_step);
+        cudaDeviceSynchronize();
+
+        cudaMemcpy(grid_host, grid_device, size_molecule, cudaMemcpyDeviceToHost);
+        cudaMemcpy(forces_host, forces_device, size_forces, cudaMemcpyDeviceToHost);
+        if (i % 10000 == 0)
+            writeVTK(i / 10000, num_molecules, grid_host);
+    }
+
+
+    cudaFree(grid_device);
+    cudaFree(forces_device);
 }
 
-std::vector<molecule> fill_points(int num_points, std::ifstream &file){
-    std::vector<molecule> grid(num_points);
-    for(auto i = 0; i < num_points; i++){
+void fill_points(Molecule *grid, int num_molecules, std::ifstream &file){
+
+    for(auto i = 0; i < num_molecules; i++){
         if(file.good()){
             std::string line;
             std::getline(file, line);
@@ -80,7 +119,6 @@ std::vector<molecule> fill_points(int num_points, std::ifstream &file){
             break; 
         }
     }
-    return grid;
 }
 
 void get_values(std::vector<double> &values, std::string line){
@@ -93,7 +131,8 @@ void get_values(std::vector<double> &values, std::string line){
 
 }
 
-void writeVTK(const char* name, int num_points, std::vector<molecule> points){
+void writeVTK(int index, int num_molecules, Molecule *points){
+    std::string name = "visualization/output_" + std::to_string(index) + ".vtk";
     std::ofstream vtk_output(name);
     vtk_output.precision(6);
         if (!vtk_output) {
@@ -105,22 +144,22 @@ void writeVTK(const char* name, int num_points, std::vector<molecule> points){
     vtk_output << "hesp visualization file\n";
     vtk_output << "ASCII\n";
     vtk_output << "DATASET UNSTRUCTURED_GRID\n";
-    vtk_output << "POINTS " <<  num_points << " double\n";
-    for(int i = 0; i < num_points; i++){
+    vtk_output << "POINTS " <<  num_molecules << " double\n";
+    for(int i = 0; i < num_molecules; i++){
         vtk_output << std::fixed << std::setprecision(6) << points[i].x
                    << " " << points[i].y << " " << points[i].z << "\n";
     }
     vtk_output << "CELLS 0 0\n";
     vtk_output << "CELL_TYPES 0\n";
-    vtk_output << "POINT_DATA " << num_points << "\n";
+    vtk_output << "POINT_DATA " << num_molecules << "\n";
     vtk_output << "SCALARS m double\n";
     vtk_output << "LOOKUP_TABLE default\n";
-    for(int i = 0; i < num_points; i++)
+    for(int i = 0; i < num_molecules; i++)
         vtk_output << std::fixed << std::setprecision(6) << 1.0 << "\n";
         
     vtk_output << "VECTORS v double\n";
 
-    for(int i = 0; i < num_points; i++){
+    for(int i = 0; i < num_molecules; i++){
         vtk_output << std::fixed << std::setprecision(6) << points[i].xv
                    << " " << points[i].yv << " " << points[i].zv << "\n";
     }
